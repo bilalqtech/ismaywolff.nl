893f390f44e81045870395695439980427c0d67c
ismay <ismay@users.noreply.github.com>
2016-12-27 15:43:54 +0100

    Add WorksList test (#16)

--- src/components/works/components/WorkList.test.jsx
+++ src/components/works/components/WorkList.test.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import { shallowToJson } from 'enzyme-to-json'
+import { mount } from 'enzyme'
+import { mountToJson } from 'enzyme-to-json'
 import { WorkList } from './WorkList'
 
 describe('<WorkList />', () => {
   it('renders correctly', () => {
-    const wrapper = shallow(<WorkList fetchWorks={() => {}} works={{}} />)
-    expect(shallowToJson(wrapper)).toMatchSnapshot()
+    const wrapper = mount(<WorkList fetchWorks={() => {}} works={{}} />)
+    expect(mountToJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('fetches works on mount', () => {
+    const mockFetch = jest.fn()
+    mount(<WorkList fetchWorks={mockFetch} works={{}} />)
+    expect(mockFetch).toBeCalled()
   })
 })
