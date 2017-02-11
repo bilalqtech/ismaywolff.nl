67d3f7107276a65821a5bf298fa30c288f914778
ismay <ismay@users.noreply.github.com>
2017-02-11 16:24:16 +0100

    Add stylelint (#149)

--- client/components/navigation/components/Navigation.jsx
+++ client/components/navigation/components/Navigation.jsx
@@ -5,9 +5,11 @@ import styled from 'styled-components'
 const StyledLink = styled(Link)`
   margin-left: 0.5rem;
   margin-right: 0.5rem;
+
   &:first-of-type {
     margin-left: 0;
   }
+
   &:last-of-type {
     margin-right: 0;
   }
