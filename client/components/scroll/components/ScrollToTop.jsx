f7bcb12374a33a8d74c9103e3e4b7dfd72186a78
ismay <ismay@users.noreply.github.com>
2017-04-01 15:00:56 +0200

    Fix scroll to top (#268)
    * Fix scroll to top
    
    * Updated scroll component
    
    * Revert temporary changes

--- client/components/scroll/components/ScrollToTop.jsx
+++ client/components/scroll/components/ScrollToTop.jsx
@@ -2,16 +2,12 @@ import { Component, PropTypes } from 'react'
 import { withRouter } from 'react-router'
 
 /**
- * Scrolls window to top on navigation for clients that don't
- * yet support scroll restoration. This component can be removed
- * when enough clients support it.
+ * Scrolls window to top on navigation
  */
 
 export class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
-    const hasScrollRestoration = window.history.scrollRestoration
-
-    if (!hasScrollRestoration && this.props.location !== prevProps.location) {
+    if (this.props.location !== prevProps.location) {
       window.scrollTo(0, 0)
     }
   }
--- client/components/scroll/components/ScrollToTop.test.jsx
+++ client/components/scroll/components/ScrollToTop.test.jsx
@@ -9,12 +9,7 @@ describe('<ScrollToTop />', () => {
     expect(shallowToJson(wrapper)).toMatchSnapshot()
   })
 
-  it('scrolls to top on route changes for clients without scroll restoration', () => {
-    Object.defineProperty(window.history, 'scrollRestoration', {
-      writable: true,
-      value: false
-    })
-
+  it('scrolls to top on route changes', () => {
     Object.defineProperty(window, 'scrollTo', {
       writable: true,
       value: jest.fn()
@@ -24,22 +19,5 @@ describe('<ScrollToTop />', () => {
 
     wrapper.setProps({ location: { url: 'new' } })
     expect(window.scrollTo).toHaveBeenCalledWith(0, 0)
-  })
-
-  it('ignores clients with scroll restoration', () => {
-    Object.defineProperty(window.history, 'scrollRestoration', {
-      writable: true,
-      value: true
-    })
-
-    Object.defineProperty(window, 'scrollTo', {
-      writable: true,
-      value: jest.fn()
-    })
-
-    const wrapper = mount(<ScrollToTop location={{ url: 'old' }} />)
-
-    wrapper.setProps({ location: { url: 'new' } })
-    expect(window.scrollTo).not.toHaveBeenCalled()
   })
 })
