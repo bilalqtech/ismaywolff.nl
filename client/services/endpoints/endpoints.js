828de742658dcd1d0e7a040d1a5be7370991b1c0
ismay <ismay@users.noreply.github.com>
2017-04-02 15:56:24 +0200

    Show works in reverse data order (#275)

--- client/services/endpoints/endpoints.js
+++ client/services/endpoints/endpoints.js
@@ -3,5 +3,5 @@ const isProd = process.env.NODE_ENV === 'production'
 const DOMAIN = isProd ? 'cdn' : 'preview'
 const BASE = `https://${DOMAIN}.contentful.com/spaces/${process.env.SPACE_ID}`
 
-export const WORKS = `${BASE}/entries?content_type=works`
+export const WORKS = `${BASE}/entries?content_type=works&order=-fields.published`
 export const IMAGES = `${BASE}/assets?mimetype_group=image`
