f79544605fd088700efeb45365f9f4bcf696df1c
ismay <hello@ismaywolff.nl>
2017-05-24 16:26:25 +0200

    Update logerror (#471)
    * Push errors to queue if Raven hasn't loaded yet
    
    * Push error in format expected by error-events

--- src/shared/services/raven/logError.js
+++ src/shared/services/raven/logError.js
@@ -1,4 +1,5 @@
-/* global Raven */
+/* global Raven, window */
+/* eslint-disable no-underscore-dangle */
 
 /**
  * Logs any errors passed to it to Sentry. Accepts additional data (see docs below).
@@ -7,11 +8,16 @@
  */
 
 function logError(error, additional = {}) {
+  const hasErrorEvents = typeof window === 'object' && '__ERROR_EVENTS__' in window
   const hasRaven = typeof Raven !== 'undefined' && 'captureException' in Raven
   const isProd = process.env.NODE_ENV === 'production'
 
-  if (isProd && hasRaven) {
-    Raven.captureException(error, additional)
+  if (isProd) {
+    if (hasRaven) {
+      Raven.captureException(error, additional)
+    } else if (hasErrorEvents) {
+      window.__ERROR_EVENTS__({ error })
+    }
   }
 
   if (!isProd) {
